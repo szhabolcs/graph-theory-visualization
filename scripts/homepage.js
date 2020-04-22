a30e9c0afe8f3d32bad6b7baf64359b49fe9bdcf
Nagy Szabolcs <szabi.nagy.ns@gmail.com>
2020-04-22 21:56:19 +0300

    Added smooth scrolling
    Also fixed the navbar hiding the title to scroll to by adding an offset.

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -14,3 +14,31 @@ function changeNav() {
         menuIsOpen = true;
     }
 }
+
+let nodes = document.getElementById("side-nav").childNodes[1].childNodes;
+
+for(let i=0; i<nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == 'a') {
+        nodes[i].onclick = function (e) {
+            let href = e.toElement.getAttribute("href");
+            href = href.substr(1);
+            let section = document.getElementById(href);
+            let offset = 70 + 150; // nav and offset
+
+            // get body position
+            let bodyRect = document.body.getBoundingClientRect().top; 
+            // get section position relative
+            let sectionRect = section.getBoundingClientRect().top; 
+            // subtract the section from body
+            let sectionPosition = sectionRect - bodyRect; 
+            // subtract offset
+            let offsetPosition = sectionPosition - offset; 
+            
+            e.preventDefault();
+            window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+            });
+        }
+    }
+}
