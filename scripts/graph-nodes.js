21e106efad43d8d995076203524db9fe6a4e7c48
Nagy Szabolcs <szabi.nagy.ns@gmail.com>
2020-06-03 19:06:31 +0300

    Added node selection

--- scripts/graph-nodes.js
+++ scripts/graph-nodes.js
@@ -3,6 +3,7 @@ class VisualNode {
     editMode = false;
     nodeIndex = 0;
     graph;
+    selectedNode = "n/a";
 
     /**
      * The remove button overlay on the connections
@@ -33,6 +34,7 @@ class VisualNode {
         this.jspInstance = jsPlumb.getInstance();
         this.jspInstance.setContainer(container);
         this.DOMContainer = container;
+        this.DOMMessage = $("#message");
         setJsPlumbInstanceForMotion(this.jspInstance)
 
         //Binding the event listeners
@@ -247,6 +249,52 @@ class VisualNode {
             source: connection.source,
             target: connection.target
         }).removeClass("connection-mark");
+    }
+
+    /**
+     * Shows a message to the user
+     * @param {string} message The message that's going to be displayed
+     */
+    showMessage(message){
+        this.DOMMessage.text(message);
+        this.DOMMessage.fadeIn();
+    }
+
+    /**
+     * Removes the message from the DOM
+     */
+    removeMessage(){
+        this.DOMMessage.fadeOut();
+    }
+
+    /**
+     * This function is fired when the user is in node selection mode.
+     * It sets the clicked node as the selected one.
+     * @param {Object} e the selected node
+     */
+    nodeSelect(e){
+        jspNode.selectedNode = e.currentTarget.id;
+        DOMContainer.undelegate(".node","click",this.nodeSelect);
+        DOMContainer.undelegate(".node","mouseenter mouseleave",this.nodeHover);
+        jspNode.removeMessage();
+    }
+
+    /**
+     * This function is fired when the user hovers over a node in node selection mode.
+     * Adds a border to the hovered node.
+     */
+    nodeHover(){
+        $(this).toggleClass("node-hover");
+    }
+
+    /**
+     * Initiates node selection
+     */
+    getSelectedNode(){
+        this.showMessage("Válasszon ki egy csomopontot");
+        this.selectedNode = "none";
+        this.DOMContainer.delegate(".node","click",this.nodeSelect);
+        this.DOMContainer.delegate(".node","mouseenter mouseleave",this.nodeHover);
     }
 
     resetGraph() {
